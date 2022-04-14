29a28af60d88f883a5fd8ecd8e95522ead958119
Anas Alam <anasalam027@gmail.com>
2022-04-15 01:54:52 +0530

    few more fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,12 @@ const languageRoutes = require("./language");
 const surahRoutes = require("./surah");
 const ramzanTimeTableRoutes = require("./ramzanTimeTable");
 const contentRoutes = require("./content");
+const homeRoutes = require("./home");
 
 router.use("/language", languageRoutes);
 router.use("/surah", surahRoutes);
 router.use("/ramzanTimeTable", ramzanTimeTableRoutes);
 router.use("/content", contentRoutes);
+router.use("/home", homeRoutes);
 
 module.exports = router;
